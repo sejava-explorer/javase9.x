cb8511540b63be2b87533f986c3ea7be343810ad
choice-explorer <30986019+rsharma96@users.noreply.github.com>
2019-10-20 20:35:15 +0530

    Made few changes to the code

--- src/com/explorechoice/javabasics/MethodTypes.java
+++ src/com/explorechoice/javabasics/MethodTypes.java
@@ -3,22 +3,22 @@ package com.explorechoice.javabasics;
 class MethodTypes {
 	
 	
+	
+	
 	// Variable declaration and Variable Definition
 	int x ;   
 	int y = 10;
+	
 	
 	// Method Definition and Method declaration
 //	returntype + methodname() ;
 //	
 //	void methodDeclaration();   // method declaration  // abastract method 
 
-	
-	
+
 	void methodDefinition() {
 		
 	}   // both valid 
-	
-	
 	
 	double  methodDefinitionWithReturn() {
 		double x = 45.90;
